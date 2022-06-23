1799972a6e33a43c994d171f0df63e253f0cdecf
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-23 15:40:24 -0300

    chore: Adds tailwind css dependencies and add simple style testing the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-      </header>
+      <h1 className='font-bold'>Hello world!</h1>
     </div>
   )
 }

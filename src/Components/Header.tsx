dc0dc33887d8b9df20b8798415876ec27a0de51a
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-23 19:57:19 -0300

    chore: creates header component, and delete: deletes unused mainPage

---
+++ src/Components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <div>
+      <header
+        className="border-2 mb-8 p-4"
+        style={ { backgroundColor: '#202C37' } }
+        data-testid="main-header">
+          <div className="flex justify-between">
+            <h1 className="text-3xl text-white">Where in the world?</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+            </svg>
+          </div>
+      </header>      
+    </div>
+  )
+};
+
+export default Header;

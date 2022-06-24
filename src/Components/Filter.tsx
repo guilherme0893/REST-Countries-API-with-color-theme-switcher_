9fb6127a32e86d5430a981946ae3a139a01e27b1
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-24 10:51:50 -0300

    chore: creates countries type, creates onHandleChange function and adds props to filter

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import getCountriesByRegion from '../Services/getCountriesByRegion';
 
-function Filter() {
+type countries = {
+  flag: string;
+  name: string;
+  population: number;
+  region: string;
+  capital: string;
+}
+
+interface filterProps {
+  filteredRegion: string;
+};
+
+function Filter({filteredRegion}: filterProps) {
+  const [countries, setCountries] = useState<countries[]>([]);
+  const [selectRegion, setSelectRegion] = useState('');
+
+  // https://stackoverflow.com/questions/70043668/react-typescript-input-onchange-event-type
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   console.log(event.target.value);
+  //   setSelectRegion(event.target.value.toLowerCase());
+  //   console.log(event.target.value);
+  // };
+
+  useEffect(() => {
+    getCountriesByRegion(selectRegion).then((country) => setCountries(country));
+  }, [setCountries, setSelectRegion, selectRegion]);
+
+  const onHandleChange = (value: string) => {
+    // console.log(selectRegion);
+    setSelectRegion(value)
+  }
+
   return (
     <div>
       <select
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 text-sm font-medium
-          rounded-md p-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          rounded-md p-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-2"
+        onChange={ (event) => onHandleChange(event.target.value) }
       >
-        <option className="bg-white text-black">Filter by Region</option>
+        <option className="bg-white text-black" value="">Regions</option>
         <option className="bg-white text-black" value="africa">Africa</option>
         <option className="bg-white text-black" value="america">America</option>
         <option className="bg-white text-black" value="asia">Asia</option>

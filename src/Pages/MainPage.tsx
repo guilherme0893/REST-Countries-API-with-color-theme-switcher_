be483fa1da3a3d6642c49c07b4da93241be9cb1c
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-24 10:49:04 -0300

    fix: remove navbar from mainpage and adds to the main component

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import Header from '../Components/Header';
-import Navbar from '../Components/Navbar';
 import Main from '../Components/Main';
 
 function MainPage() {
   return (
     <div>
       <Header />
-      <Navbar />
       <Main />
     </div>
   );

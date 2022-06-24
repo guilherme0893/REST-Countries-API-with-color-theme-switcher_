71bdab4b8a28ed7cf64d6a142359139f60c582bb
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-24 20:09:24 -0300

    chore: adds routes to /:id path

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import MainPage from '../Pages/MainPage';
+import DetailPages from '../Pages/DetailsPage';
 import NotFound from '../Pages/NotFound';
 
 function Routes() {
@@ -8,6 +9,7 @@ function Routes() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MainPage} />
+        <Route exact path="/:id" component={ DetailPages } />        
         <Route path="/*" component={NotFound} />
       </Switch>
     </BrowserRouter>

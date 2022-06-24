9c317efa91123d4b18512b7c6d5dcdcb4fd7e9f6
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-24 11:42:51 -0300

    chore: adds icon and function to change its color on click

--- src/Components/CountryCard.tsx
+++ src/Components/CountryCard.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-
-type countriesProps = {
-  flag: string;
-  name: string;
-  population: number;
-  region: string;
-  capital: string;
-}
+import React, { useState } from 'react';
+import countriesProps from '../Types/countriesProps';
 
 function CountryCard(props: countriesProps) {
   const { name, population, region, capital, flag } = props;
+  const [stroke, setStroke] = useState('currentColor')
+
+  const onIconClick = () => {
+    stroke === 'currentColor' ? setStroke('green') : setStroke('currentColor')
+  };
+
   return (
     <div
       data-testid='country-card'
@@ -17,8 +16,25 @@ function CountryCard(props: countriesProps) {
     >
       <div className='flex flex-col '>
         <img className='h-1/2 w-full' src={ flag } alt={ name }/>
-        <div className='m-4 text-sm'>
-          <h3 className='font-medium mb-4'>{ name }</h3>          
+        <div className='m-4 text-sm d-flex'>
+          <div className='flex'>
+            <h3 className='font-medium mb-4 mr-2'>{ name }</h3>
+            <svg
+              onClick={ onIconClick }
+              className="w-6 h-6"
+              fill="none"
+              stroke={stroke}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                >
+                </path>
+            </svg>               
+          </div>
           <div className='flex'>
             <p className='font-medium'>Population:</p>
             <p className='ml-2'>{ population }</p>

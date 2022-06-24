71a7797ceb9098ab60784e3db5fffe6b05820843
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-23 22:44:03 -0300

    chore: creates notfound page and adds it to routes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Switch, BrowserRouter} from "react-router-dom";
 import MainPage from '../Pages/MainPage';
+import NotFound from '../Pages/NotFound';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={ MainPage } />
+        <Route path="/*" component={ NotFound } />
       </Switch>      
     </BrowserRouter>
   )
